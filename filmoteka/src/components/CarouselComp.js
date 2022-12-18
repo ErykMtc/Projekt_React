fb2d2454f2442a52d50ca676f2f0deeeb89ba936
JanMKr <77554736+JanMKr@users.noreply.github.com>
2022-12-18 19:35:56 +0100

    carousels_article

--- filmoteka/src/components/CarouselComp.js
+++ filmoteka/src/components/CarouselComp.js
@@ -1,48 +1,56 @@
 import Carousel from "react-bootstrap/Carousel";
-import React, { useState } from "react";
+
+import useAuth from '../hooks/useAuth';
+import axios from 'axios';
+import { useRef, useState, useEffect, useContext } from 'react';
 
 import Img1 from "../img/img-1.jpg";
 import Img2 from "../img/img-2.jpg";
 import Img3 from "../img/img-3.jpg";
 
 export default function CarouselComp(){
+    const {auth} = useAuth();
+    const [post, setPost] = useState(null);
     const [index, setIndex] = useState(0);
 
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
     };
 
-    return (
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-                    <Carousel.Item>
-                        <img className="d-block w-100" src={Img1} alt="First slide" />
-                        <Carousel.Caption>
-                            <div className="info-panel">
-                                <h3>Slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className="d-block w-100" src={Img2} alt="Second slide" />
 
-                        <Carousel.Caption>
-                            <div className="info-panel">
-                                <h3>Slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img className="d-block w-100" src={Img3} alt="Third slide" />
+    useEffect(() => {
+        axios.get('/movies/news', {
+          headers: { 'Content-Type': 'application/json' },
+                        auth: {
+                            username: auth.user,
+                            password: auth.pwd
+                        }
+        }).then((response) => {
+          setPost(response.data);
+        });
+      }, []);
+                console.log(auth);
+                console.log(post)
+                if (!post) return null;
+    
+                return (
+                    <Carousel activeIndex={index} onSelect={handleSelect}>
+                        
+                        {post.map((item, iteration) => 
+                        <Carousel.Item>
+                                <img className="d-block w-100" src={`data:image/jpeg;base64,${item.picture}`} alt="First slide" />
+                                <Carousel.Caption>
+                                    <div className="info-panel">
+                                        <h3>{item.name}</h3>
+                                        <p>{item.description}</p>
+                                    </div>
+                                </Carousel.Caption>
+                        </Carousel.Item>
+            
+                                )}
+            
+                        
+                    </Carousel>
+                )
 
-                        <Carousel.Caption>
-                            <div className="info-panel">
-                                <h3>Slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-    )
 }
